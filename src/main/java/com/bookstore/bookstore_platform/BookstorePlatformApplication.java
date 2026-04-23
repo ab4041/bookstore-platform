aa7f883b0dcc097a0fd7478b0e4b91c2bcb4ac16
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-23 21:27:05 +0530

    Added microservices module structure

---
+++ src/main/java/com/bookstore/bookstore_platform/BookstorePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstore_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/bookstore_platform/BookstorePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstore_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
